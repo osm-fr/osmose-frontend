0aa309fd4eeabd297ce9e19c2b5c9bd84750bdd2
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-05-28 18:45:54 +0200

    Fix update script to handle new items

--- tools/database/item_menu_update.sh
+++ tools/database/item_menu_update.sh
@@ -2,5 +2,6 @@
 
 for i in item_menu*.txt; do
   echo $i
-  cat $i | sed "s/'/''/g" | sed "s/ *\([0-9]*\) *| \(.*\)/update dynpoi_item set menu = menu || hstore('nl', '\2') where item = \1;/" | psql osmose
+  lang=`echo $i | cut -d'_' -f3 | cut -d. -f1`
+  cat $i | sed "s/'/''/g" | sed "s/ *\([0-9]*\) *| \(.*\)/update dynpoi_item set menu = coalesce(menu, hstore('$lang','')) || hstore('$lang', '\2') where item = \1;/" | psql osmose
 done
