3847b921331ae72eab1878c4c8fa0c2c279449b0
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-07-29 22:30:47 +0200

    Add cron.sh, which should be called by cron every day

---
+++ tools/cron.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+set -e
+
+DATABASE=osmose
+
+psql -d $DATABASE -c "DELETE FROM dynpoi_status WHERE date < now()-interval '7 day' AND status = 'done';"
+
+psql -d $DATABASE -c "INSERT INTO dynpoi_stats (SELECT dynpoi_class.source, dynpoi_class.class, now(), count(marker.source) FROM dynpoi_class LEFT JOIN marker ON dynpoi_class.source=marker.source AND dynpoi_class.class=marker.class GROUP BY dynpoi_class.source, dynpoi_class.class);"
+
+psql -d $DATABASE -c "UPDATE dynpoi_item SET levels = (SELECT array_agg(level)
+              FROM (SELECT level FROM dynpoi_class
+                    WHERE dynpoi_class.item = dynpoi_item.item
+                    GROUP BY level
+                    ORDER BY level
+                   ) AS a
+             );"
