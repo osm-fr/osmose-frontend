712a15753b9d0d2f11f0174b393a0e77ea850987
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2012-10-29 22:38:03 +0100

    Fix SQL request to get number of errors per item/level

--- tools/cron.sh
+++ tools/cron.sh
@@ -17,7 +17,9 @@ psql -d $DATABASE -c "UPDATE dynpoi_item SET levels = (SELECT array_agg(level)
              );"
 
 psql -d $DATABASE -c "UPDATE dynpoi_item SET number = (SELECT array_agg(n)
-              FROM (SELECT count(*) AS n FROM dynpoi_class
+              FROM (SELECT count(*) AS n FROM marker
+                    JOIN dynpoi_class ON dynpoi_class.source = marker.source AND
+                                         dynpoi_class.class = marker.class
                     WHERE dynpoi_class.item = dynpoi_item.item
                     GROUP BY level
                     ORDER BY level
