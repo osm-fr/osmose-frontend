a007f4398e60dff904bd33d11131ea51ff2d0c88
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2013-06-12 22:07:11 +0200

    Improve update-passwords.py

--- tools/update-passwords.py
+++ tools/update-passwords.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 
+import os
 import sys
-from pyPgSQL import PgSQL
 import utils
 
 dbconn = utils.get_dbconn()
@@ -15,6 +15,13 @@ def update_pass(comment, update, contact="Jocelyn Jaubert <jocelyn@osm1.crans.or
   global source
 
   try:
+    dbcurs.execute("SELECT source, comment, update FROM dynpoi_source WHERE comment=%s;",
+                   (comment, ))
+    if dbcurs.rowcount == 1:
+      prev_update = dbcurs.fetchone()["update"]
+      if prev_update == update:
+        return
+
     # try to update password for an analyse
     dbcurs.execute("UPDATE dynpoi_source SET update=%s WHERE comment=%s;",
                    (update, comment))
@@ -25,10 +32,14 @@ def update_pass(comment, update, contact="Jocelyn Jaubert <jocelyn@osm1.crans.or
     print "failure on comment=%s update=%s" % (comment, update)
     raise
 
-#  if dbcurs.rowcount == 0:
-    # try to update name for an analyse for a given password
+  if dbcurs.rowcount == 0:
+#    # try to update name for an analyse for a given password
 #    dbcurs.execute("UPDATE dynpoi_source SET comment=%s WHERE update=%s;",
 #                   (comment, update))
+    pass
+  else:
+    print "updated update=%s where comment=%s" % (update, comment)
+    return
 
   if dbcurs.rowcount == 0:
     # otherwise, create a new entry in database
@@ -41,6 +52,10 @@ def update_pass(comment, update, contact="Jocelyn Jaubert <jocelyn@osm1.crans.or
     except PgSQL.OperationalError:
       print "failure on comment=%s update=%s" % (comment, update)
       return
+
+  else:
+    print "updated comment=%s where update=%s" % (comment, update)
+    return
 
 sys.path.append("../../backend")
 import osmose_config
