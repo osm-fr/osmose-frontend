fb953a3f2a3828e30dfcaf60efeeffdecdd1d63c
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-02-08 23:06:53 +0100

    Don't update menu item when translation is missing from file

--- tools/database/item_menu_update.sh
+++ tools/database/item_menu_update.sh
@@ -3,5 +3,5 @@
 for i in item_menu*.txt; do
   echo $i
   lang=`echo $i | cut -d'_' -f3 | cut -d. -f1`
-  cat $i | sed "s/'/''/g" | sed "s/ *\([0-9]*\) *| \(.*\)/update dynpoi_item set menu = coalesce(menu, hstore('$lang','')) || hstore('$lang', '\2') where item = \1;/" | psql osmose_frontend
+  cat $i | grep "[0-9]* | ." | sed "s/'/''/g" | sed "s/ *\([0-9]*\) *| \(.*\)/update dynpoi_item set menu = coalesce(menu, hstore('$lang','')) || hstore('$lang', '\2') where item = \1;/" | psql osmose_frontend
 done
