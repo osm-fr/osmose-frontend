ea4ab02a7724b9fc1fb499132dec1760e4961d62
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-02-08 23:07:12 +0100

    Remove hack around socket.getaddrinfo to force ipv4 as some backends are ipv6-only

--- tools/update.py
+++ tools/update.py
@@ -34,15 +34,6 @@ class printlogger:
     def log(self, text):
         print text
 
-
-origGetAddrInfo = socket.getaddrinfo
-
-def getAddrInfoWrapper(host, port, family=0, socktype=0, proto=0, flags=0):
-   return origGetAddrInfo(host, port, socket.AF_INET, socktype, proto, flags)
-
-# replace the original socket.getaddrinfo by our version
-socket.getaddrinfo = getAddrInfoWrapper
-
 ###########################################################################
 ## updater
 
