f0abfcbd29bd022ed517a9a31de89325200eb6ca
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-02-22 19:31:20 +0100

    Osmose.Errors.js, Leflat already bind function

--- static/map/Osmose.Errors.js
+++ static/map/Osmose.Errors.js
@@ -9,20 +9,19 @@ OsmoseErrors = L.LayerGroup.extend({
   initialize: function (menu) {
     L.LayerGroup.prototype.initialize.call(this);
     this._menu = menu;
-    this._updateOsmoseLayerBind = this._updateOsmoseLayer.bind(this);
   },
 
   onAdd: function (map) {
     this._map = map;
-    map.on('moveend', this._updateOsmoseLayerBind, this);
-    this._menu.on('itemchanged', this._updateOsmoseLayerBind, this);
+    map.on('moveend', this._updateOsmoseLayer, this);
+    this._menu.on('itemchanged', this._updateOsmoseLayer, this);
     this._onMap = true;
     this._updateOsmoseLayer();
   },
 
   onRemove: function (map) {
-    map.off('moveend', this._updateOsmoseLayerBind, this);
-    this._menu.off('itemchanged', this._updateOsmoseLayerBind, this);
+    map.off('moveend', this._updateOsmoseLayer, this);
+    this._menu.off('itemchanged', this._updateOsmoseLayer, this);
     this._onMap = false;
     this.clearLayers();
   },
