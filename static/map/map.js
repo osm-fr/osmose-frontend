35b8f259d75e15337ee48511f6b871d10238cf10
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2014-03-05 23:45:04 +0100

    Fix no overlay detection

--- static/map/map.js
+++ static/map/map.js
@@ -50,7 +50,7 @@ function init_map() {
   function getUrlVars() {
     var vars = [],
       hash;
-    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
     for (var i = 0; i < hashes.length; i++) {
       hash = hashes[i].split('=');
       vars.push(hash[0]);
