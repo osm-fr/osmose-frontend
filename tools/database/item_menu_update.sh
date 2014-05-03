897ea220ee5c3e8ae242214c470ef2edfc7a4b34
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2014-05-03 23:32:49 +0200

    Update database for menu from .po files

--- tools/database/item_menu_update.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-for i in item_menu*.txt; do
-  echo $i
-  lang=`echo $i | cut -d'_' -f3 | cut -d. -f1`
-  cat $i | grep "[0-9]* | ." | sed "s/'/''/g" | sed "s/ *\([0-9]*\) *| \(.*\)/update dynpoi_item set menu = coalesce(menu, hstore('$lang','')) || hstore('$lang', '\2') where item = \1;/" | psql osmose_frontend
-done
---
+++ tools/menu_update.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+import codecs
+import os
+import polib
+import utils
+
+class OsmoseTranslation:
+
+    def __init__(self):
+        self.languages = []
+        self.trans = {}
+        for fn in os.listdir("../po/"):
+            if not fn.endswith(".po"):
+                continue
+
+            l = fn[:-3]
+            self.languages.append(l)
+            po = polib.pofile("../po/" + l + ".po")
+            self.trans[l] = {}
+            for entry in po:
+                if entry.msgstr != "":
+                    self.trans[l][entry.msgid] = entry.msgstr
+
+    def translate(self, str, args=()):
+        out = {}
+        out["en"] = str % args   # english version
+        for l in self.languages:
+            if str in self.trans[l] and self.trans[l][str] != "":
+                out[l] = self.trans[l][str] % args
+        return out
+
+t = OsmoseTranslation()
+
+dbconn = utils.get_dbconn()
+dbcurs = dbconn.cursor()
+
+types = ("categ", "item")
+
+for typ in types:
+  sql = "update dynpoi_" + typ + " set menu = coalesce(menu, hstore(%s,''))|| hstore(%s, %s) where categ = %s;"
+
+  for line in codecs.open("database/" + typ + "_menu.txt", "r", "utf-8"):
+    (item, s) = line.split("|")
+    item = int(item)
+    s = s.strip()[3:-2]
+    translations = t.translate(s)
+    for (l, s) in translations.iteritems():
+      dbcurs.execute(sql, (l, l, s, item))
+
+dbconn.commit()
