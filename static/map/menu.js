1197494683a48bfdbe20961463cc6280ca7e7e3a
Frederic Rodrigo <fred.rodrigo@gmail.com>
2016-02-06 18:38:22 +0100

    Decode url params, avoid double encoding. github #24

--- static/map/menu.js
+++ static/map/menu.js
@@ -17,7 +17,7 @@ function getUrlVars() {
     var hashes = window.location.href.slice(window.location.href.indexOf('#') + 1).split('&');
     for (var i = 0; i < hashes.length; i++) {
       hash = hashes[i].split('=');
-      vars[hash[0]] = hash[1];
+      vars[decodeURI(hash[0])] = decodeURI(hash[1]);
     }
   }
   return vars;
