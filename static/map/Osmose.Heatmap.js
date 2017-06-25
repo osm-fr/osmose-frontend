c6a49a7da316981c02e152b6f9a3214dd3e0c5b0
Frederic Rodrigo <fred.rodrigo@gmail.com>
2017-06-25 16:20:20 +0200

    Support setUrl on L.VectorGrid.Protobuf

--- static/map/Osmose.Heatmap.js
+++ static/map/Osmose.Heatmap.js
@@ -54,6 +54,10 @@ OsmoseHeatmap = L.VectorGrid.Protobuf.extend({
   },
 
   _setUrl: function () {
-    this.setUrl(this._makeUrl()); // setUrl is not implemented https://github.com/Leaflet/Leaflet.VectorGrid/issues/49
+    // setUrl is not implemented
+    // https://github.com/Leaflet/Leaflet.VectorGrid/issues/49
+    // https://github.com/Leaflet/Leaflet.VectorGrid/pull/105
+    this._url = this._makeUrl();
+    this.redraw();
   },
 });
