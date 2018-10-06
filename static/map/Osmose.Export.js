3e85eb18c048f048f4888f15afd4afc7aee91536
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-10-06 15:20:18 +0000

    Fix export link to folow bbox move

--- static/map/Osmose.Export.js
+++ static/map/Osmose.Export.js
@@ -5,14 +5,23 @@ const OsmoseExport = L.Class.extend({
 
   _map: null,
 
+  _params_last: {},
+
   initialize(map, permalink, params) {
     this._map = map;
     permalink.on('update', this._setUrl, this);
+    map.on('moveend', (e) => {
+      this._setUrl({ params: this._params_last });
+    });
+    this._params_last = params;
     this._setUrl({ params });
   },
 
   _setUrl(e) {
     const params = Object.assign({}, e.params);
+    delete params.lat;
+    delete params.lon;
+    this._params_last = params;
     params.limit = 500;
     params.bbox = this._map.getBounds().toBBoxString();
     $('#menu-export ul a').each((i, a) => {
