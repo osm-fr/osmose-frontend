21c0ce21e4ad899f4388c932eee7271b3e49464c
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-10-27 09:23:49 +0000

    Fix cron.sh after add new table 'class'

--- tools/cron.sh
+++ tools/cron.sh
@@ -13,7 +13,11 @@ psql -d $DATABASE -c "DELETE FROM dynpoi_status WHERE date < now()-interval '7 d
 psql -d $DATABASE -c "INSERT INTO dynpoi_stats (SELECT dynpoi_class.source, dynpoi_class.class, now(), count(marker.source) FROM dynpoi_class LEFT JOIN marker ON dynpoi_class.source=marker.source AND dynpoi_class.class=marker.class GROUP BY dynpoi_class.source, dynpoi_class.class);"
 
 psql -d $DATABASE -c "UPDATE dynpoi_item SET levels = (SELECT array_agg(level)
-              FROM (SELECT level FROM dynpoi_class
+              FROM (SELECT level
+                    FROM dynpoi_class
+                      JOIN class ON
+                        class.item = dynpoi_class.item AND
+                        class.class = dynpoi_class.class
                     WHERE dynpoi_class.item = dynpoi_item.item
                     GROUP BY level
                     ORDER BY level
@@ -21,12 +25,20 @@ psql -d $DATABASE -c "UPDATE dynpoi_item SET levels = (SELECT array_agg(level)
              );"
 
 psql -d $DATABASE -c "UPDATE dynpoi_item SET number = (SELECT array_agg(n)
-              FROM (SELECT count(*) AS n FROM marker
-                    JOIN dynpoi_class ON dynpoi_class.source = marker.source AND
-                                         dynpoi_class.class = marker.class
-                    WHERE dynpoi_class.item = dynpoi_item.item
-                    GROUP BY level
-                    ORDER BY level
+              FROM (SELECT count(*) AS n
+                    FROM marker
+                      JOIN dynpoi_class ON
+                        dynpoi_class.source = marker.source AND
+                        dynpoi_class.class = marker.class
+                      JOIN class ON
+                        class.item = dynpoi_class.item AND
+                        class.class = dynpoi_class.class
+                    WHERE
+                      dynpoi_class.item = dynpoi_item.item
+                    GROUP BY
+                      level
+                    ORDER BY
+                      level
                    ) AS a
              );"
 
@@ -34,8 +46,15 @@ psql -d $DATABASE -c "UPDATE dynpoi_item SET tags = (SELECT array_agg(tag)
               FROM (
                 SELECT
                     tag
-                FROM
-                    (SELECT unnest(tags) AS tag, item FROM dynpoi_class WHERE dynpoi_class.item = dynpoi_item.item) AS dynpoi_class
+                FROM (
+                  SELECT unnest(tags) AS tag, dynpoi_class.item
+                  FROM
+                    dynpoi_class
+                    JOIN class ON
+                      class.item = dynpoi_class.item AND
+                      class.class = dynpoi_class.class
+                  WHERE
+                    dynpoi_class.item = dynpoi_item.item) AS dynpoi_class
                 WHERE
                     tag != ''
                 GROUP BY tag
