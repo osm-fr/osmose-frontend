1f05c02af68d954e3396d4d4c5606786c22663ff
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2018-10-28 16:17:24 +0000

    Change sql code style of cron.sh

--- tools/cron.sh
+++ tools/cron.sh
@@ -8,9 +8,19 @@ DIR_DUMP="/data/work/$(whoami)/"
 
 # Update various tables in database
 
-psql -d $DATABASE -c "DELETE FROM dynpoi_status WHERE date < now()-interval '7 day' AND status = 'done';"
+psql -d $DATABASE -c "
+DELETE FROM dynpoi_status
+WHERE date < now()-interval '7 day' AND status = 'done';
+"
 
-psql -d $DATABASE -c "INSERT INTO dynpoi_stats (SELECT dynpoi_class.source, dynpoi_class.class, now(), count(marker.source) FROM dynpoi_class LEFT JOIN marker ON dynpoi_class.source=marker.source AND dynpoi_class.class=marker.class GROUP BY dynpoi_class.source, dynpoi_class.class);"
+psql -d $DATABASE -c "
+INSERT INTO dynpoi_stats (
+  SELECT dynpoi_class.source, dynpoi_class.class, now(), count(marker.source)
+  FROM dynpoi_class
+    LEFT JOIN marker ON dynpoi_class.source=marker.source AND dynpoi_class.class=marker.class
+  GROUP BY dynpoi_class.source, dynpoi_class.class
+);
+"
 
 psql -d $DATABASE -c "
 UPDATE dynpoi_item SET levels = (
