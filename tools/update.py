bf7cd66ac9bce3db712025e667a8a5dd8473bc9e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-08-17 16:11:50 +0200

    Update subtitle from new errors using uuid

--- tools/update.py
+++ tools/update.py
@@ -87,36 +87,12 @@ def update(source_id, fname, logger = printlogger(), remote_ip=""):
 UPDATE
   dynpoi_status
 SET
-  subtitle = marker.subtitle,
-  lat = marker.lat,
-  lon = marker.lon
-FROM
-  marker
-WHERE
-  marker.source = %s AND
-  marker.elems != '' AND
-  dynpoi_status.source = marker.source AND
-  dynpoi_status.class = marker.class AND
-  dynpoi_status.subclass = marker.subclass AND
-  dynpoi_status.elems = marker.elems
-""", (source_id, ))
-
-    execute_sql(dbcurs, """
-UPDATE
-  dynpoi_status
-SET
   subtitle = marker.subtitle
 FROM
   marker
 WHERE
   marker.source = %s AND
-  marker.elems = '' AND
-  dynpoi_status.source = marker.source AND
-  dynpoi_status.class = marker.class AND
-  dynpoi_status.subclass = marker.subclass AND
-  dynpoi_status.elems = marker.elems AND
-  dynpoi_status.lat = marker.lat AND
-  dynpoi_status.lon = marker.lon
+  dynpoi_status.uuid = marker.uuid
 """, (source_id, ))
 
     ## remove false positive no longer present
