a6bf2e89875bba97c998ef2f3094f507df3f71b5
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2019-12-08 21:29:45 +0000

    Fix and clean close issue using api 0.2

--- tools/osmose_common.py
+++ tools/osmose_common.py
@@ -10,31 +10,28 @@ def remove_bug_err_id(error_id, status):
   PgCursor = PgConn.cursor()
 
   # find source
-  PgCursor.execute("SELECT source,class,subclass,elems,lat,lon FROM marker WHERE id = %s", (error_id, ))
+  PgCursor.execute("SELECT uuid,source,class FROM marker WHERE id = %s", (error_id, ))
   source_id = None
   for res in PgCursor.fetchall():
+      uuid = res["uuid"]
       source_id = res["source"]
       class_id = res["class"]
-      sub_class = res["subclass"]
-      elems = res["elems"]
-      lat = res["lon"]
-      lon = res["lat"]
 
   if not source_id:
       return -1
 
-  PgCursor.execute("DELETE FROM dynpoi_status WHERE marker_id=%s", (error_id, ))
+  PgCursor.execute("DELETE FROM dynpoi_status WHERE uuid=%s", (uuid, ))
 
   PgCursor.execute("""INSERT INTO dynpoi_status
                         (id,source,class,subclass,elems,date,status,lat,lon,subtitle,uuid)
                       SELECT id,source,class,subclass,elems,NOW(),%s,
                              lat,lon,subtitle,uuid
                       FROM marker
-                      WHERE id = %s
+                      WHERE uuid = %s
                       ON CONFLICT DO NOTHING""",
-                   (status, error_id))
+                   (status, uuid))
 
-  PgCursor.execute("DELETE FROM marker WHERE id = %s", (error_id, ))
+  PgCursor.execute("DELETE FROM marker WHERE uuid = %s", (uuid, ))
   PgCursor.execute("UPDATE dynpoi_class SET count = count - 1 WHERE source = %s AND class = %s;", (source_id, class_id))
   PgConn.commit()
 
@@ -47,15 +44,11 @@ def remove_bug_uuid(uuid, status):
   PgCursor = PgConn.cursor()
 
   # find source
-  PgCursor.execute("SELECT source,class,subclass,elems,lat,lon FROM marker WHERE uuid = %s", (uuid, ))
+  PgCursor.execute("SELECT source,class FROM marker WHERE uuid = %s", (uuid, ))
   source_id = None
   for res in PgCursor.fetchall():
       source_id = res["source"]
       class_id = res["class"]
-      sub_class = res["subclass"]
-      elems = res["elems"]
-      lat = res["lon"]
-      lon = res["lat"]
 
   if not source_id:
       return -1
