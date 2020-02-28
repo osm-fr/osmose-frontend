1629bd4d8dbd3b02145d3e7a969e45f7aa22c838
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2020-02-28 22:17:37 +0100

    Allow multiple color by menu categ for markers

--- tools/add-markers.py
+++ tools/add-markers.py
@@ -3,6 +3,7 @@
 
 all_flags = ["O", "L", "K", "P", "M", "F", "=", "|", "||", "::", ".:.", "T", "t", "X", "><", "L'", "[]", ".", ".l", "'.", "/", "=-", "H", "h"]
 
+import pprint
 import utils
 
 if __name__ == "__main__":
@@ -10,37 +11,60 @@ if __name__ == "__main__":
   dbconn = utils.get_dbconn()
   dbcurs = dbconn.cursor()
 
-  sql = "select marker.item from marker left join dynpoi_item on dynpoi_item.item = marker.item where dynpoi_item.item IS NULL group by marker.item order by marker.item;"
+  sql = """
+CREATE TEMP TABLE marker_list_item AS
+WITH RECURSIVE t AS (
+   (SELECT item FROM marker ORDER BY item LIMIT 1)  -- parentheses required
+   UNION ALL
+   SELECT (SELECT item FROM marker WHERE item > t.item ORDER BY item LIMIT 1)
+   FROM t
+   WHERE t.item IS NOT NULL
+   )
+SELECT item FROM t WHERE item IS NOT NULL;
+"""
+  dbcurs.execute(sql)
+
+  sql = """
+select m.item 
+from marker_list_item m
+left join dynpoi_item on dynpoi_item.item = m.item
+where dynpoi_item.item IS NULL
+order by m.item;"""
   dbcurs.execute(sql)
 
   prev_cat = ""
 
   for res in dbcurs.fetchall():
   #  print res
-    i = int(res[0])
-    c = int(i / 1000) * 10
-    if prev_cat != c:
-        prev_cat = c
-        avail_flags = all_flags[:]
+    item = int(res[0])
+    categ = int(item / 1000) * 10
 
-    sql = "select item, marker_color, marker_flag from dynpoi_item where categ = %s order by item"
-    dbcurs.execute(sql, (c,))
-    for m in dbcurs.fetchall():
-  #    print m
-      color = m["marker_color"]
-      if m["marker_flag"] in avail_flags:
-        avail_flags.remove(m["marker_flag"])
+    if prev_cat != categ:
+      prev_cat = categ
+      colors = set()
+      avail_flags = {}
+      sql = "select item, marker_color, marker_flag from dynpoi_item where categ = %s order by item"
+      dbcurs.execute(sql, (categ,))
+      for m in dbcurs.fetchall():
+        color = m["marker_color"]
+        colors.add(color)
+        if color not in avail_flags:
+          avail_flags[color] = all_flags[:]
+        if m["marker_flag"] in avail_flags[color]:
+          avail_flags[color].remove(m["marker_flag"])
 
-  #  print "  missing %d" % i
-  #  print "possible flags:", avail_flags
+    chosen_color = None
+    chosen_flag = None
+    for c in colors:
+      if len(avail_flags[c]) > 0:
+        chosen_color = c
+        chosen_flag = avail_flags[c][0]
+        continue
 
-    if len(avail_flags) == 0:
-      print "not enough available flags for item=%d" % i
+    if not chosen_color:
+      print "not enough available flags for item=%d" % item
       continue
 
-  #  en = raw_input("English: ")
-  #  fr = raw_input("French: ")
-
-    print "insert into dynpoi_item values (%d, %d, '%s', '%s', NULL, ARRAY[1, 2, 3]);" % (i, c, color, avail_flags[0].replace("'", "''"))
-    avail_flags.remove(avail_flags[0])
+    print "insert into dynpoi_item values (%d, %d, '%s', '%s', NULL, ARRAY[1, 2, 3]);" % (item, categ, chosen_color, chosen_flag.replace("'", "''"))
+    avail_flags[chosen_color].remove(chosen_flag)
 
