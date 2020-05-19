f468755e5ea9c4461a72015f1d7e1081c8fdd923
Francesco Ansanelli <francians@gmail.com>
2020-05-19 18:11:40 +0200

    fix python 3.7 warnings

--- modules/bottle_gettext.py
+++ modules/bottle_gettext.py
@@ -64,7 +64,7 @@ class GettextPlugin(object):
 
         # Test if the original callback accepts a 'lang' keyword.
         # Ignore it if it does not need a gettext handle.
-        args = inspect.getargspec(route.callback)[0]
+        args = inspect.getfullargspec(route.callback)[0]
         if keyword not in args:
             return callback
 
