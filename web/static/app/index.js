8e6b8ffa264e6d40087fdcb65744063461284200
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-01-09 20:03:29 +0100

    Add numeral to format number

--- web/static/app/index.js
+++ web/static/app/index.js
@@ -2,10 +2,13 @@ import Vue from 'vue'
 import { router } from './router.js'
 import { i18n, loadLanguageAsync } from './i18n.js'
 import SortedTablePlugin from "vue-sorted-table"
+import numeral from 'numeral'
+import numFormat from 'vue-filter-number-format'
 
 import App from './app.vue'
 
 Vue.use(SortedTablePlugin);
+Vue.filter('numFormat', numFormat(numeral));
 
 document.addEventListener("DOMContentLoaded", (event) => {
     router.beforeEach((to, from, next) => {
