24e7914b0634acfd35b8f5238f8612b80305cadb
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-01-23 00:10:51 +0100

    vue: repackage i18n for gettext

---
+++ web/static/app/components/translate-slot.vue
@@ -0,0 +1,26 @@
+<template>
+  <i18n tag="span" :path="text">
+    <template v-for="(index, name) in $slots" v-slot:[name]>
+      <slot :name="name" />
+    </template>
+  </i18n>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      text: null,
+    };
+  },
+  mounted() {
+    this.text = this.$slots.default[0].children[0].text;
+    this.text = this.text
+      .replace(/(\r\n|\n|\r)/gm, " ")
+      .replace(/ +/gm, " ")
+      .trim();
+  },
+});
+</script>
