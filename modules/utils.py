b1e0175d9428a79c411415012e58269e24a32def
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-01-24 16:29:51 +0000

    fix i18n

--- modules/utils.py
+++ modules/utils.py
@@ -138,7 +138,7 @@ def i10n_select(translations, langs):
         if 'en' in translations:
             return {'auto': translations['en']}
         else:
-            return {'auto': translations.values()[0]}
+            return {'auto': list(translations.values())[0]}
 
 def i10n_select_auto(translations, langs):
     if translations:
