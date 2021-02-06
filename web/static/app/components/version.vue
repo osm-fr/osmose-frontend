d1d46c34009519e59d8df94e1d37d012cdad4f7e
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-02-06 16:41:02 +0100

    vue: fix update_summary_by_analyser

--- web/static/app/components/version.vue
+++ web/static/app/components/version.vue
@@ -1,14 +1,21 @@
-<template>
-  <div :class="v !== '' && v != max ? 'version-warning' : null">
-    <slot></slot>
-  </div>
-</template>
-
 <script>
 import Vue from "vue";
 
 export default Vue.extend({
   props: ["v", "max"],
+  render: function (createElement) {
+    if (this.v) {
+      return createElement(
+        this.$vnode.data.tag,
+        {
+          class: this.v !== "" && this.v != this.max ? "version-warning" : null,
+        },
+        this.$slots.default
+      );
+    } else {
+      return createElement(this.$vnode.data.tag);
+    }
+  },
 });
 </script>
 
