1043a7a134290ae2ae55296a85a688f9048a8c2b
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2021-05-15 23:18:23 +0200

    enable eslint:recommended

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: [
     // add more generic rulesets here, such as:
-    // 'eslint:recommended',
+    'eslint:recommended',
     // 'plugin:vue/vue3-recommended',
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
     'plugin:vue/essential' // Use this if you are using Vue.js 2.x.
@@ -9,5 +9,9 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+  },
+  globals: {
+    "API_URL": false,
+    "L": false,
   }
 }
