84b56952385c28db3230959cdd78ea87d945e24f
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-11-19 20:35:09 +0100

    Disable eslint vue/multi-word-component-names

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    'vue/multi-word-component-names': 0
   },
   globals: {
     "API_URL": false,
