2887452af163dcf7a13b0e26536e1a9bdec5e5a8
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-12-04 17:13:43 +0100

    web: byuser fix links layout

--- web/src/pages/byuser/byuser.vue
+++ web/src/pages/byuser/byuser.vue
@@ -25,30 +25,31 @@
           </ul>
           <div class="form-inline my-2 my-lg-0">
             <a :href="byuser_count" class="badge badge-secondary">
-              .rss <translate>count</translate>
-            </a>
+              .rss <translate>count</translate> </a
+            >&nbsp;
             <a :href="api_url_path('rss')" class="badge badge-secondary">
-              .rss
-            </a>
+              .rss </a
+            >&nbsp;
             <a :href="api_url_path('gpx')" class="badge badge-secondary">
-              .gpx
-            </a>
+              .gpx </a
+            >&nbsp;
             <a :href="api_url_path('kml')" class="badge badge-secondary">
-              .kml
-            </a>
+              .kml </a
+            >&nbsp;
             <a :href="api_url_path('csv')" class="badge badge-secondary">
-              .csv
-            </a>
-            <a :href="api_url_path()" class="badge badge-secondary"> .json </a>
+              .csv </a
+            >&nbsp;
+            <a :href="api_url_path()" class="badge badge-secondary"> .json </a
+            >&nbsp;
             <a
               :href="api_url_path(undefined, 'full=true')"
               class="badge badge-secondary"
             >
-              .json full
-            </a>
+              .json full </a
+            >&nbsp;
             <a :href="api_url_path('geojson')" class="badge badge-secondary">
-              .geojson
-            </a>
+              .geojson </a
+            >&nbsp;
             <a
               :href="api_url_path('geojson', 'full=true')"
               class="badge badge-secondary"
